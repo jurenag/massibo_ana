627eb751c574c853e6c3465bee1f8e45d9f67061
Julio <jurenag@outlook.es>
2024-09-26 13:23:28 +0200

    Format custom_exceptions.py using Black formatter by Microsoft

--- src/utils/custom_exceptions.py
+++ src/utils/custom_exceptions.py
@@ -7,6 +7,7 @@ class MalFunction(Exception):
     def why(self):
         return "This exception is meant for situations where the code is not working as expected, but the reason is not clear."
 
+
 class InvalidParameterDefinition(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -15,7 +16,8 @@ class InvalidParameterDefinition(Exception):
     @classmethod
     def why(self):
         return "This exception is meant for situations where a parameter was given an invalid value. This check has to do with the parameter per se. No other parameter comes into play for this check."
-    
+
+
 class InconsistentParametersDefinition(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -24,7 +26,8 @@ class InconsistentParametersDefinition(Exception):
     @classmethod
     def why(self):
         return "This exception is meant for situations where a set of parameters which are not compatible among each other was given."
-    
+
+
 class InsufficientParameters(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -33,7 +36,8 @@ class InsufficientParameters(Exception):
     @classmethod
     def why(self):
         return "This exception is meant for situations where not even a minimal set of parameters has been defined."
-    
+
+
 class NoAvailableData(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -42,7 +46,8 @@ class NoAvailableData(Exception):
     @classmethod
     def why(self):
         return "This exception is meant for situations where the requested information is not available."
-    
+
+
 class TypeException(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -52,6 +57,7 @@ class TypeException(Exception):
     def why(self):
         return "This exception is meant for situations where the provided object type is not allowed."
 
+
 class ShapeException(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -60,7 +66,8 @@ class ShapeException(Exception):
     @classmethod
     def why(self):
         return "This exception is meant for situations where the provided multidimensional object, say an array, does not have a valid shape."
-    
+
+
 class WfmReadException(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -69,7 +76,8 @@ class WfmReadException(Exception):
     @classmethod
     def why(self):
         return "This exception is meant for situations where something went wrong reading a .WFM file (binary waveform file for Tektronix performance oscilloscope)."
-    
+
+
 class InvalidPhysicalConfiguration(Exception):
     def __init__(self, message):
         super().__init__(message)
