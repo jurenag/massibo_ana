ff024ae78eceb19b947cd80fc379a58b85852632
Julio <jurenag@outlook.es>
2024-10-03 17:27:06 +0200

    Propagate the latest documentation changes to the ASCII gain data case

--- src/preprocess/DataPreprocessor.py
+++ src/preprocess/DataPreprocessor.py
@@ -860,30 +860,24 @@ class DataPreprocessor:
             # aux_gainmeas_dict['date'][:10], gives 'YYYY-MM-DD'.
             output_filepath_base = f"{aux_gainmeas_dict['strip_ID']}-{aux_gainmeas_dict['sipm_location']}-{aux_gainmeas_dict['thermal_cycle']}-OV{round(10.*aux_gainmeas_dict['overvoltage_V'])}dV-{aux_gainmeas_dict['date'][:10]}"
 
-            # Not removing the following chunk of code because you will probably 
-            # re-use this when including the shutil.move functionality here
-            # ---------------------------------------------------------------------
-            _, extension = os.path.splitext(
-                aux["raw_filepath"] ## This one probably needs to be changed with the preprocessing restructuring
-            )  # Preserve the original extension
-            new_raw_filename = output_filepath_base + "_raw_gain" + extension
-            _ = DataPreprocessor.rename_file(
-                aux["raw_filepath"], new_raw_filename, overwrite=True, verbose=verbose ## This one probably needs to be changed with the preprocessing restructuring
-            )
-            # ---------------------------------------------------------------------
+            _, extension = os.path.splitext(self.ASCIIGainCandidates[key])
 
-            # You probably also need to add a 'timestamp_filepath' new entry to this dictionary
-            # at this level, because in this context (generate_meas_config_files()) you have
-            # successfully paired up ASCII core data files with their corresponding timestamps.
-            # ---------------------------------------------------------------------
+            new_raw_filepath = os.path.join(
+                data_folderpath, 
+                output_filepath_base + "_raw_gain" + extension)
+            
+            shutil.move(
+                self.ASCIIGainCandidates[key], 
+                new_raw_filepath
+            )
+
             aux_wvfset_dict.update(
                 {
-                    "timestamp_filepath": os.path.relpath( ## However, instead of updating, just consider adding a new entry
-                        new_processed_filepath, start=root_directory
+                    "wvf_filepath": os.path.relpath(
+                        new_raw_filepath, start=root_directory
                     )
                 }
             )
-            # ---------------------------------------------------------------------
 
             wvf_output_filepath = os.path.join(
                 aux_folderpath, output_filepath_base + "_gain_wvf.json"
