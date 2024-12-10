8bd9521fea49b675b25aa08dea0a762f22b4f925
Julio <jurenag@outlook.es>
2024-12-10 13:20:00 +0100

    Style changes to src/utils/custom_exceptions.py

--- src/utils/custom_exceptions.py
+++ src/utils/custom_exceptions.py
@@ -5,7 +5,9 @@ class MalFunction(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where the code is not working as expected, but the reason is not clear."
+        return "This exception is meant for situations where the " \
+        "code is not working as expected, but the reason is not " \
+        "clear."
 
 
 class InvalidParameterDefinition(Exception):
@@ -15,7 +17,10 @@ class InvalidParameterDefinition(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where a parameter was given an invalid value. This check has to do with the parameter per se. No other parameter comes into play for this check."
+        return "This exception is meant for situations where a " \
+        "parameter was given an invalid value. This check has to " \
+        "do with the parameter per se. No other parameter comes " \
+        "into play for this check."
 
 
 class InconsistentParametersDefinition(Exception):
@@ -25,7 +30,8 @@ class InconsistentParametersDefinition(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where a set of parameters which are not compatible among each other was given."
+        return "This exception is meant for situations where a set " \
+        "of parameters which are not compatible among each other was given."
 
 
 class InsufficientParameters(Exception):
@@ -35,7 +41,8 @@ class InsufficientParameters(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where not even a minimal set of parameters has been defined."
+        return "This exception is meant for situations where not " \
+        "even a minimal set of parameters has been defined."
 
 
 class NoAvailableData(Exception):
@@ -45,7 +52,8 @@ class NoAvailableData(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where the requested information is not available."
+        return "This exception is meant for situations where the " \
+        "requested information is not available."
 
 
 class TypeException(Exception):
@@ -55,7 +63,8 @@ class TypeException(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where the provided object type is not allowed."
+        return "This exception is meant for situations where the " \
+        "provided object type is not allowed."
 
 
 class ShapeException(Exception):
@@ -65,7 +74,9 @@ class ShapeException(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where the provided multidimensional object, say an array, does not have a valid shape."
+        return "This exception is meant for situations where the " \
+        "provided multidimensional object, say an array, does not " \
+        "have a valid shape."
 
 
 class WfmReadException(Exception):
@@ -75,7 +86,9 @@ class WfmReadException(Exception):
 
     @classmethod
     def why(self):
-        return "This exception is meant for situations where something went wrong reading a .WFM file (binary waveform file for Tektronix performance oscilloscope)."
+        return "This exception is meant for situations where something " \
+        "went wrong reading a .WFM file (binary waveform file for " \
+        "Tektronix performance oscilloscope)."
 
 
 class InvalidPhysicalConfiguration(Exception):
