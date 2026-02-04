4c28fc2c4e3a0d5a321483c13f02bde165ba0d36
Julio <jurenag@outlook.es>
2026-02-04 13:20:42 +0100

    Implement data download from cernbox web

--- ci/aux/b0_download_data.sh
+++ ci/aux/b0_download_data.sh
@@ -8,13 +8,19 @@ fi
 
 SET_TO_DOWNLOAD="$1"
 
-# Navigate to the input_data folder
-cd ci_workspace/input_data
-
 # Download the data
-# wget -r --reject="index.html*" -e robots=off https://massibo.web.cern.ch/clean_set_${SET_TO_DOWNLOAD}/
-# For now, just copy from a local path
-cp -r /Users/jurenag/cernbox/www/massibo/clean_set_${SET_TO_DOWNLOAD}/ .
-
-# Navigate back to the repository root folder
-cd ../..
+# - Recursive download
+# - Avoid recursive search in parent directories
+# - Do not create the host directory (massibo.web.cern.ch/...)
+# - Remove the first directory level (clean_set_X/)
+# - Download to ci_workspace/input_data
+# - Do not download index.html files
+wget \
+  -r \
+  -np \
+  -nH \
+  --cut-dirs=1 \
+  -P ci_workspace/input_data \
+  --reject="index.html*" \
+  -e robots=off \
+  https://massibo.web.cern.ch/clean_set_${SET_TO_DOWNLOAD}/
